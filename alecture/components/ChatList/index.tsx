0a9bd88374dbb9ae46545da8581146fa317927cb
hyukim <woo1038@hanmail.net>
2022-07-27 15:06:03 +0900

    update

--- alecture/components/ChatList/index.tsx
+++ alecture/components/ChatList/index.tsx
@@ -1,24 +1,34 @@
 import Chat from '@components/Chat';
 import { IDM } from '@typings/db';
-import React, { useCallback, useRef, VFC } from 'react';
+import React, { forwardRef, RefObject, useCallback, VFC } from 'react';
 import Scrollbars from 'react-custom-scrollbars';
 import { ChatZone, Section, StickyHeader } from './styles';
 
 interface Props {
   chatSections: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
+  scrollRef: RefObject<Scrollbars>;
 }
 
-const ChatList: VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null);
+const ChatList: VFC<Props> = ({ chatSections, setSize, isEmpty, isReachingEnd, scrollRef }) => {
   const onScroll = useCallback((values) => {
-    if (values.scrollTop === 0) {
+    if (values.scrollTop === 0 && !isReachingEnd) {
       console.log('가장위');
+      setSize((prevSize) => prevSize + 1).then((res) => {
+        console.log(scrollRef.current?.getScrollHeight(), values.scrollHeight);
+        if (scrollRef?.current) {
+          scrollRef.current?.scrollTop(scrollRef.current?.getScrollHeight() - values.scrollHeight);
+          // scrollbarRef.current?.scrollTop(scrollbarRef.current?.getScrollHeight() - values.scrollHeight);
+        }
+      });
     }
   }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
         {Object.entries(chatSections)?.map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
--- alecture/pages/DirectMessage/index.tsx
+++ alecture/pages/DirectMessage/index.tsx
@@ -2,9 +2,9 @@ import Workspace from '@layouts/Workspace';
 import { Container } from '@pages/Channel/styles';
 import { Header } from '@pages/SignUp/styles';
 import fetcher from '@utils/fetcher';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router';
-import useSWR from 'swr';
+import useSWR, { useSWRInfinite } from 'swr';
 import gravatar from 'gravatar';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
@@ -12,40 +12,65 @@ import useInput from '@hooks/useInput';
 import { IDM } from '@typings/db';
 import axios from 'axios';
 import makeSection from '@utils/makeSection';
+import Scrollbars from 'react-custom-scrollbars';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
-  const { data: chatData, mutate: mutateChat, revalidate } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const { data: chatData, mutate: mutateChat, revalidate, setSize } = useSWRInfinite<IDM[]>(
+    (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
     fetcher,
   );
-  console.log(111, chatData);
   const [chat, onChangeChat, setChat] = useInput('');
-
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+  const scrollbarRef = useRef<Scrollbars>(null);
   const onSubmitForm = useCallback(
     (e) => {
       e.preventDefault();
 
-      if (chat?.trim()) {
+      if (chat?.trim() && chatData) {
+        const savedChat = chat;
+        mutateChat((prevChatData) => {
+          prevChatData?.[0].unshift({
+            id: (chatData[0][0]?.id || 0) + 1,
+            content: savedChat,
+            SenderId: myData.id,
+            Sender: myData,
+            ReceiverId: userData.id,
+            Receiver: userData,
+            createdAt: new Date(),
+          });
+          return prevChatData
+        }, false).then(() => {
+          setChat('');
+          scrollbarRef.current?.scrollToBottom();
+        });
+
         axios
           .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
             content: chat,
           })
           .then(() => {
             revalidate();
-            setChat('');
           })
           .catch(console.error);
       }
     },
-    [chat],
+    [chat, chatData],
   );
+
+  // 로딩시 스크롤바 제일 아래로
+  useEffect(() => {
+    if (chatData?.length === 1) {
+      scrollbarRef.current?.scrollToBottom();
+    }
+  }, [chatData]);
 
   if (!userData || !myData) return null;
 
-  const chatSections = makeSection(chatData ? [...chatData].reverse() : []);
+  const chatSections = makeSection(chatData ? [...chatData.flat()].reverse() : []);
 
   return (
     <Container>
@@ -53,7 +78,13 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections} />
+      <ChatList
+        chatSections={chatSections}
+        scrollRef={scrollbarRef}
+        setSize={setSize}
+        isEmpty={isEmpty}
+        isReachingEnd={isReachingEnd}
+      />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="" />
     </Container>
   );
